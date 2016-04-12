80bd4223c00b5341a95047d4d39821d0e9e65f3a
zuaa <zuaa@163.com>
2016-04-12 17:02:15 +0800

    删除不必要文件的版本控制

--- src/test/java/cn/weeego/test/Testhession.java
+++ src/test/java/cn/weeego/test/Testhession.java
@@ -3,15 +3,29 @@ package cn.weeego.test;
 import java.net.MalformedURLException;
 
 import com.caucho.hessian.client.HessianProxyFactory;
+import com.gecpp.fm.IFuzzySearch;
+import com.gecpp.fm.QueryResult;
 
 public class Testhession {
 	public static void main(String[] args) {
+
 		HessianProxyFactory factory = new HessianProxyFactory();
-		HelloService api;
+		IFuzzySearch api;
 		try {
-			api = (HelloService) factory.create(HelloService.class,
-					"http://zuaa:aaaaaaaa@127.0.0.1:8080/HelloService");
-			System.out.println(api.sayHello());
+			api = (IFuzzySearch) factory.create(IFuzzySearch.class,
+					"http://113.106.92.172/fm/portal");
+
+			String[] parts = new String[2];
+			parts[0] = ("VJ0805Y103JXXPW1BC_____10");
+			parts[1] = ("15000018_____10");
+			QueryResult queryResult = api.getProductByMultipleSearch(parts);
+
+			System.out.println(queryResult.getPidListGroupMfs1());
+
+			System.out.println(queryResult.getPidListGroupMfs2());
+
+			System.out.println(queryResult.getPidListGroupMfs3());
+
 		} catch (MalformedURLException e) {
 
 			e.printStackTrace();
